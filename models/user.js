60c518be46736d262c75bf1d8364c8a8bb110bb2
therewasaguy <jason.sigal@gmail.com>
2016-11-07 13:09:58 -0500

    passport-twitter

--- models/user.js
+++ models/user.js
@@ -3,7 +3,8 @@ var Schema = mongoose.Schema;
 var passportLocalMongoose = require('passport-local-mongoose');
 
 var User = new Schema({
-  role: String
+  role: String,
+  twitterId: String
 });
 
 // plugin adds username, hash and salt for password
